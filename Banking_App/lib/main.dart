68f02f290054ed81aa0d5229694b1ea12dc2e276
Ram Budha <devops.rambudha@gmail.com>
2020-07-30 10:25:19 +0530

    squash! Add Banking app along with my resume app

---
+++ Banking_App/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(HomePage());
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Banking App'),
+          actions: <Widget>[
+            Icon(Icons.info_outline),
+          ],
+        ),
+      ),
+    );
+  }
+}
